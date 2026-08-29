c49ef85c6bfca5def12a76d662e4fecad97348e2
Mark Hatch <58341254+mark2382@users.noreply.github.com>
2026-08-29 08:05:24 -0700

    Update __init__.py

--- scustomtkinter/__init__.py
+++ scustomtkinter/__init__.py
@@ -13,9 +13,9 @@ def get_asset_path(relative_path: str) -> str:
 
 from .sctk_core import sCTk
 from .sctk_button_primary import sCTkButtonPrimary
-from . import sCTkButtonPrimarybo
+from . import sctk_button_primarybo
 __all__ = [
-    "sCTkButtonPrimarybo"
+    "sctk_button_primarybo"
 ]
 from .sctk_button_secondary import sCTkButtonSecondary
 from .sctk_button_tertiary import sCTkButtonTertiary
